238b1ad0fec61ac233516f4027ef7ffd4307802a
Jonnadatanvi <jonnadatanvi2811@gmail.com>
2023-09-17 19:17:37 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+// Budget API
+
 const express = require('express');
-const app= express()
-const port= 3000;
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 25
+        },
+        {
+            title: 'Rent',
+            budget: 275
+        },
+        {
+            title: 'Grocery',
+            budget: 110
+        },
+    ]
+};
+
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening0+ at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
